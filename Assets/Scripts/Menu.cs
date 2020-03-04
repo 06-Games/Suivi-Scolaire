783031e5441aec837fe71c1225aa87673f62ba51
EvanG <evan_g@orange.fr>
2020-03-04 17:42:41 +0100

    Side menu animation

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    RectTransform Panel;
+    Vector2 InitialPos;
+    float SecondsPerPixel;
+
+    void Awake()
+    {
+        Panel = (RectTransform)transform.Find("Panel");
+        InitialPos = Panel.anchoredPosition;
+        Panel.anchoredPosition = new Vector2(-InitialPos.x, InitialPos.y);
+        SecondsPerPixel = 0.25F / (InitialPos.x * 2);
+    }
+
+    IEnumerator Mvt(Vector2 objective, System.Action onComplete = null)
+    {
+        float dist = objective.x - Panel.anchoredPosition.x;
+        float time = Mathf.Abs(dist) * SecondsPerPixel;
+        var sw = new System.Diagnostics.Stopwatch();
+        sw.Start();
+
+        var initial = Panel.anchoredPosition.x;
+        while (sw.Elapsed.TotalSeconds < time)
+        {
+            Panel.anchoredPosition = new Vector2(initial + (dist / time * (float)sw.Elapsed.TotalSeconds), Panel.anchoredPosition.y);
+            yield return new WaitForEndOfFrame();
+        }
+        sw.Stop();
+        Panel.anchoredPosition = objective;
+        onComplete?.Invoke();
+    }
+
+    void OnEnable() { StartCoroutine(Mvt(InitialPos)); }
+    public void Hide() { StartCoroutine(Mvt(new Vector2(-InitialPos.x, InitialPos.y), () => gameObject.SetActive(false))); }
+}
