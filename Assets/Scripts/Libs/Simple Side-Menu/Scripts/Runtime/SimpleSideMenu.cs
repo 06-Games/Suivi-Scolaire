516e9331e8a8ca91cac2ae1bb3e7e0184570ba5c
EvanG <evan_g@orange.fr>
2020-03-11 22:19:52 +0100

    Bug fix: menu adapts to changes in resolution
    I had to realize it afterwards 😭

--- Assets/Scripts/Libs/Simple Side-Menu/Scripts/Runtime/SimpleSideMenu.cs
+++ Assets/Scripts/Libs/Simple Side-Menu/Scripts/Runtime/SimpleSideMenu.cs
@@ -75,6 +75,7 @@ namespace DanielLochner.Assets.SimpleSideMenu
         }
         private void Update()
         {
+            rectTransform.sizeDelta = new Vector2(400, Screen.height / rectTransform.parent.localScale.y);
             OnStateUpdate();
             OnOverlayUpdate();
         }
