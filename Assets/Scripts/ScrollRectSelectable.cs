4efe0c4bd070a9bbde931985647e5b30b8f45a2d
EvanG <evan_g@orange.fr>
2020-11-17 19:24:07 +0100

    ScrollRectButton -> ScrollRectSelectable

---
+++ Assets/Scripts/ScrollRectSelectable.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ScrollRectSelectable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler, IScrollHandler
+{
+    ScrollRect m_TargetScrollRect;
+    void Start() => m_TargetScrollRect = transform.GetComponentInParent<ScrollRect>();
+
+    //Drag
+    public bool Drag = true;
+    public void OnBeginDrag(PointerEventData eventData) { if (Drag) m_TargetScrollRect.SendMessage("OnBeginDrag", eventData); }
+    public void OnDrag(PointerEventData eventData) { if (Drag) m_TargetScrollRect.SendMessage("OnDrag", eventData); }
+    public void OnEndDrag(PointerEventData eventData) { if (Drag) m_TargetScrollRect.SendMessage("OnEndDrag", eventData); }
+
+    //Scroll
+    public void OnScroll(PointerEventData data) => m_TargetScrollRect.SendMessage("OnScroll", data);
+}
