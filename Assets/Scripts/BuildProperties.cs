2b4fe0b2f461621f68550ccfe1eccd31eaf3a22b
EvanG <evan_g@orange.fr>
2021-01-11 11:46:40 +0100

    It's now possible to access the application documents on iOS

---
+++ Assets/Scripts/BuildProperties.cs
@@ -0,0 +1,26 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEditor.iOS.Xcode;
+using UnityEngine;
+
+public class BuildProperties : MonoBehaviour
+{
+    [PostProcessBuild]
+    public static void ChangeXcodePlist(BuildTarget buildTarget, string pathToBuiltProject)
+    {
+        if (buildTarget != BuildTarget.iOS) return;
+        // Get plist
+        string plistPath = pathToBuiltProject + "/Info.plist";
+        PlistDocument plist = new PlistDocument();
+        plist.ReadFromFile(plistPath);
+        PlistElementDict rootDict = plist.root;
+
+        // Change values
+        rootDict.SetBoolean("UIFileSharingEnabled", true); // https://developer.apple.com/documentation/bundleresources/information_property_list/uifilesharingenabled
+        rootDict.SetBoolean("LSSupportsOpeningDocumentsInPlace", true); // https://developer.apple.com/documentation/bundleresources/information_property_list/lssupportsopeningdocumentsinplace
+
+        plist.WriteToFile(plistPath); // Write plist
+    }
+}
+#endif
